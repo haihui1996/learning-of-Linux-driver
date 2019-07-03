9d5c610b7f3d34700cd37276c3b14baa838e390b
haihui <haihui_deng@163.com>
2019-07-03 17:02:19 +0800

    上传参考代码

---
+++ first_drv/first_drv.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/delay.h>
+#include <asm/uaccess.h>
+#include <asm/irq.h>
+#include <asm/io.h>
+#include <asm/arch/regs-gpio.h>
+#include <asm/hardware.h>
+
+static struct class *firstdrv_class;
+static struct class_device	*firstdrv_class_dev;
+
+volatile unsigned long *gpbcon = NULL;
+volatile unsigned long *gpbdat = NULL;
